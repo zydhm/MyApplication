a1d983e77fba2e10c7e1146a89a59e2834c63ad2
zhongyuandong3 <yuandong3@eastmoney.com>
2019-11-26 09:10:29 +0800

    别的分支提交

--- app/src/main/java/com/example/myapplication/ClickActivity.java
+++ app/src/main/java/com/example/myapplication/ClickActivity.java
@@ -15,7 +15,7 @@ import com.airbnb.lottie.LottieAnimationView;
  * 简书:    http://www.jianshu.com/u/61f41588151d
  * github:  https://github.com/panacena
  */
-public class ClickActivity extends AppCompatActivity {
+public class ClickActivity extends AppCompatActivity {//test
 
     private Button button1,button2;
     private TextView tv_seek;
